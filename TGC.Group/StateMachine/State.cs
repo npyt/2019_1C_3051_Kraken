333a35662fc0bbeb0eb4ae03dba0856bc8088d53
Dante Bado <dantebado@gmail.com>
2019-06-07 16:40:59 -0300

    State Machine

---
+++ TGC.Group/StateMachine/State.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TGC.Core.Camara;
+using TGC.Core.Example;
+using TGC.Core.Input;
+using TGC.Group.Camara;
+using TGC.Group.Model;
+
+namespace TGC.Group.StateMachine
+{
+    abstract class State
+    {
+        protected GameModel parent;
+
+        public State(GameModel mparent)
+        {
+            parent = mparent;
+        }
+
+        public abstract void init();
+        public abstract void update(float ElapsedTime);
+        public abstract void render(float ElapsedTime);
+        public abstract void dispose();
+    }
+}
